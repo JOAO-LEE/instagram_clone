fa8ee400aa5468762e7d1c136f15c554eb3fe56c
João Lee <joaumlimaum@gmail.com>
2024-02-21 17:02:51 -0300

    Created aside navigation bar structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+            protocol: "https",
+            port: "",
+            hostname:  "upload.wikimedia.org",
+            pathname: "/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
+            },
+        ]
+    }
+};
+// https://png.pngtree.com
 export default nextConfig;
