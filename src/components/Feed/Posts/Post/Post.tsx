3d27051a8f2d6c1cf7a1bb58e551d880e2ebf3e1
João Lee <joaumlimaum@gmail.com>
2024-02-22 23:39:53 -0300

    Created first comment section layout

--- src/components/Feed/Posts/Post/Post.tsx
+++ src/components/Feed/Posts/Post/Post.tsx
@@ -1,5 +1,5 @@
 import { PostDTO } from "@/model/Post.dto";
-import { BookmarkSimple, ChatCircle, DotsThree, Heart, PaperPlaneTilt } from "@phosphor-icons/react/dist/ssr";
+import { BookmarkSimple, ChatCircle, DotsThree, Heart, PaperPlaneTilt, Smiley } from "@phosphor-icons/react/dist/ssr";
 
 export default function Post({username, postCaption, userImage, postImage}: PostDTO) {
    
@@ -10,20 +10,22 @@ export default function Post({username, postCaption, userImage, postImage}: Post
             <p className="font-bold flex-1">{username}</p>
         <DotsThree size={32} className="h-5" />
         </header>
-        {/* <div> */}
             <img src={postImage} alt={`${username} post`} className="object-cover w-full" />
-            <p className="font-bold">{username}</p>
-        {/* </div> */}
-
-        <div className="flex justify-between px-4">
-            <div className="flex space-x-4">
+        <div className="flex justify-between">
+            <div className="flex">
                 <Heart className="action-buttons"/>
                 <ChatCircle className="action-buttons"/>
                 <PaperPlaneTilt className="action-buttons"/>
             </div>
         <BookmarkSimple className="action-buttons"/>
         </div>
+        <p className="p-5 truncate"><span className="font-bold mr-2">{username}</span>{postCaption}</p>
+        <form className="flex items-center p-4 box-border m-0">
+            <input type="text" name="" id="" placeholder="Add a comment" className="flex-1 border-none focus:ring-0"/>
+            <button className="text-blue-400 font-bold" >Post</button>
+            <Smiley size={30}/> 
+        </form>
+
      </section>
     )
 }
-
