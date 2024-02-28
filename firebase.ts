40a0d62e807d0d4ff1acdb215fc49c331f509d58
João Lee <joaumlimaum@gmail.com>
2024-02-28 17:57:53 -0300

    Changed .env variables - accidentally pushed last time =/

--- firebase.ts
+++ firebase.ts
@@ -1,13 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
@@ -15,14 +9,11 @@ const firebaseConfig = {
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID, 
   appId: process.env.APP_ID, 
-  measurementId: process.env.MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig): getApp();
 const db = getFirestore();
 const storage = getStorage();
 
 export { app, db, storage };
 
-// const analytics = getAnalytics(app);
