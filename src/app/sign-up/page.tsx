065027f0b6405d15322a593a7228e5aeb95b4983
João Lee <joaumlimaum@gmail.com>
2024-02-29 12:28:53 -0300

    More styles at Sign up component

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -2,7 +2,7 @@ import {SignUp} from "../../components/SignUp/SignUp";
 
 export default function Page() {
     return ( 
-        <main className="w-full bg-orange-800 min-h-screen">
+        <main className="w-full min-h-screen flex items-center">
             <SignUp />
         </main>
         
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -4,28 +4,38 @@ import Image from "next/image"
 
 export function SignUp() {
     return (
-       <section className="bg-white p-5 ">
-            <div className="flex justify-center items-center space-x-10">
-                <div>
-                    <Image src={"https://echoinnovateit.com/wp-content/uploads/2023/07/instagram-like-app-development.png"} width={400} height={400} alt="Instagram demonstration"/>
+       <section className="p-5 w-full">
+            <div className="flex justify-center items-center">
+                <div className="hidden lg:inline-block">
+                    <Image src={"https://echoinnovateit.com/wp-content/uploads/2023/07/instagram-like-app-development.png"} width={500} height={500} alt="Instagram demonstration"/>
                 </div>
-                <form action="" className="border rounded border-gray-300 p-5 flex flex-col items-center">
+                <form action="" className="border rounded border-gray-300 p-10 flex flex-col items-center">
                     <Image 
                     width={150}
                     height={150} 
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png" 
                     alt="Instagram Logo" 
                     className="object-contain"/>
-                    <div className="flex flex-col space-y-3">
-                        <label htmlFor="">Username</label>
-                        <input type="text" placeholder="" />
-                        <label htmlFor="">Password</label>
-                        <input type="password" />
-                        <button>Sign up</button>
+                    <div className="flex flex-col space-y-3 text-xs p-5">
+                        <div className="bg-slate-200 flex flex-col border rounded border-gray-300 p-1">
+                            <label htmlFor="">Phone number, username, or email</label>
+                            <input type="text" className="border-none  bg-slate-200 focus:ring-0"/>
+                        </div>
+                        <div className="bg-slate-200 flex flex-col border rounded border-gray-300 p-1">
+                            <label htmlFor="">Password</label>
+                            <input type="password" className="border-none  bg-slate-200 focus:ring-0" />
+                        </div>
+                        <button 
+                        className="bg-blue-400 text-white rounded-md min-h-6 p-2 hover:bg-blue-600 font-bold">
+                            Sign up
+                        </button>
                     </div>
-                    <p>This app was created for learning purposes.</p>
                 </form>
             </div>
+            <div className="border rounded border-gray-300 p-8 text-center text-sm">
+                <p>Don't have an account? <span className="font-bold text-blue-400 cursor-pointer">Sign up</span></p>
+            </div>
+            <p className="text-xs text-center">This app was created for learning purposes.</p>
        </section>
     )
 }
