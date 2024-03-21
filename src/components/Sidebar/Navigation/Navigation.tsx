75b56d3e6aed61debf0d10f9052150d48313d57b
João Lee <joaumlimaum@gmail.com>
2024-03-21 02:18:02 -0300

    Changed sidebar styles and added profile section with user image from active session

--- src/components/Sidebar/Navigation/Navigation.tsx
+++ src/components/Sidebar/Navigation/Navigation.tsx
@@ -3,52 +3,51 @@ import { Compass, FilmSlate, Heart, House, MagnifyingGlass, MessengerLogo, Plus 
 import { useModalState } from "../../../../store/modalState"
 import { ResolvingMetadata, Metadata } from "next"
 import { Props } from "react-modal"
-
-export function generateMetadata(): Metadata {
-    return {
-      title: 'Create post - Instagram Clone'
-    }
-
-}
+import { useSession } from "next-auth/react"
+// import { use } from "react"
 
 export default function Navigation() {
     const { action } = useModalState();
-  
-     const openModal = () => {
-        // if
-        action()
-     }
+    const {data: session } = useSession();
     
     return (
-        <ul className="flex flex-col gap-7 p-1.5">
-            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300">
-                <House size={30} />
-                <p className="pl-4 sm:hidden md:inline lg:inline">Home</p>
+        <ul className="flex flex-col gap-7">
+            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                <House className="text-2xl" />
+                <p className="sm:hidden md:inline lg:inline">Home</p>
             </li>
-            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300">
-                <MagnifyingGlass size={30} />
-                <p className="pl-4 sm:hidden md:inline lg:inline">Search</p>
+            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                <MagnifyingGlass className="text-2xl" />
+                <p className="sm:hidden md:inline lg:inline">Search</p>
             </li>
-            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300">
-                <Compass size={30} />
-                <p className="pl-4 sm:hidden md:inline lg:inline">Explore</p>
+            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                <Compass className="text-2xl" />
+                <p className="sm:hidden md:inline lg:inline">Explore</p>
             </li>
-            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300">
-                <FilmSlate size={30} />
-                <p className="pl-4 sm:hidden md:inline lg:inline">Reels</p>
+            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                <FilmSlate className="text-2xl" />
+                <p className="sm:hidden md:inline lg:inline">Reels</p>
             </li>
-            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300">
-                <MessengerLogo size={30} />
-                <p className="pl-4 sm:hidden md:inline lg:inline">Messages</p>
+            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                <MessengerLogo className="text-2xl" />
+                <p className="sm:hidden md:inline lg:inline">Messages</p>
             </li>
-            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300">
-                <Heart size={30} />
-                <p className="pl-4 sm:hidden md:inline lg:inline">Notifications</p>
+            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                <Heart className="text-2xl" />
+                <p className="sm:hidden md:inline lg:inline">Notifications</p>
             </li>
-            <li  className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300" onClick={openModal}>
-                <Plus size={30} />
-                <p className="pl-4 sm:hidden md:inline lg:inline">Create</p>
+            <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4" onClick={action}>
+                <Plus className="text-2xl" />
+                <p className="sm:hidden md:inline lg:inline">Create</p>
+            </li>
+           <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                <img src={session?.user.image!} alt="User profile image" className="h-6 rounded-full" />
+                <p className="sm:hidden md:inline lg:inline">Profile</p>
             </li>
         </ul>
     )
 }
+
+// [&_li]:flex [&_li]:items-center [&_li]:rounded-sm [&_li]:hover:bg-gray-100 [&_li]:transition-colors [&_li]:text-lg [&_li]:sm:text-md
+// flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm
+// flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -6,8 +6,8 @@ import ThreadsIcon from "@/assets/icons/ThreadsIcon";
 export default function Sidebar() {
     return ( 
         <nav className="hidden sm:inline-block sticky top-0 max-h-svh border border-b-0 border-l-0 box-border">
-         <div className="flex flex-col p-4 h-lvh box-border">
-                <div className="cursor-pointer h-24 w-[103px] relative hidden md:inline-grid lg:inline-grid">
+            <div className="flex flex-col p-4 h-lvh box-border">
+                <div className="cursor-pointer h-24 w-[103px] relative hidden md:inline-grid lg:inline-grid p-1">
                     <Image 
                     width={100}
                     height={100} 
@@ -16,25 +16,31 @@ export default function Sidebar() {
                     className="object-contain"/>
                 </div>
                 <div className="flex flex-col items-center lg:hidden md:hidden mt-4 mb-10">
-                <InstagramLogo size={30} />
+                    <InstagramLogo className="text-2xl" />
                 </div>
-            <div className="grow">
-                <Navigation />
-            </div>
-            <div className="flex flex-col gap-2">
-                <div className="flex rounded-sm hover:bg-gray-100 transition-colors duration-300">
-                <ThreadsIcon  width={30}/>
-                <p className="pl-4 sm:hidden md:inline lg:inline">Threads</p>
+                <div className="grow">
+                    <Navigation />
                 </div>
-                <div className="flex rounded-sm hover:bg-gray-100 transition-colors duration-300">
-                    <List size={30} />
-                    <p className="pl-4 sm:hidden md:inline lg:inline">More</p>
-                </div>
-            </div>
-        </div> 
+                <ul className="flex flex-col gap-3.5">
+                    <li className="hidden 2xl:flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                        <ThreadsIcon  width={25}/>
+                        <p className="sm:hidden md:inline lg:inline">Threads</p>
+                    </li>
+                    <li className="flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4">
+                        <List className="text-2xl" />
+                        <p className="sm:hidden md:inline lg:inline">More</p>
+                    </li>
+                </ul>
+            </div> 
         </nav>
     )
 }
 
+// flex items-center rounded-sm hover:bg-gray-100 transition-colors duration-300 sm:text-sm p-1 md:gap-4
+
 // nav responsivity (see it later)
 // h-screen md:w-[244px] lg:w-[335px] sm:w-[4.5em] m-0
+{/*
+<div className="flex rounded-sm hover:bg-gray-100 transition-colors duration-300">
+
+</div> */}
