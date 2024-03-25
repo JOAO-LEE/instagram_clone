4d0cc6dddf2a600b5cf027b247cf953dfeb48938
João Lee <joaumlimaum@gmail.com>
2024-03-25 12:09:14 -0300

    Upload modal now shows selected image

--- src/components/Modals/UploadModal/UploadModal.tsx
+++ src/components/Modals/UploadModal/UploadModal.tsx
@@ -2,16 +2,35 @@
 import ReactModal from "react-modal";
 import { useModalState } from "../../../../store/modalState";
 import { ImageSquare, Video } from "@phosphor-icons/react";
+import { ChangeEvent, useState } from "react";
+import { TrashSimple } from "@phosphor-icons/react/dist/ssr";
 
 export default function UploadModal() {
   const { isOpen, action } = useModalState();
+  const [selectedFile, setSelectedFile] = useState<any>();
+
+  const addImageToPost = (e: ChangeEvent<HTMLInputElement>) => {
+    const fileReader = new FileReader();
+    const isThereAFile = e?.target?.files![0];
+    if (isThereAFile) {
+      fileReader.readAsDataURL(isThereAFile);
+    }
+    fileReader.onload = (readerEvent) => {
+     setSelectedFile(readerEvent.target?.result); 
+    }
+
+  }
+
   return (
     <ReactModal
       overlayClassName=""
       className="max-w-2xl w-[50%] md:w-3/4 h-[400px] sm:h-[500px] md:h-[750px] bg-white shadow-md shadow-gray-800 flex flex-col focus:outline-none border border-gray-300 rounded-lg"
       isOpen={isOpen}
       ariaHideApp={false}
-      onRequestClose={() => action()}
+      onRequestClose={() => {
+        action();
+        setSelectedFile(null)
+      }}
       shouldCloseOnEsc={true}
       style={{
         overlay: {
@@ -27,15 +46,25 @@ export default function UploadModal() {
       </header>
       <main className="flex justify-center items-center relative h-full">
         <section className="flex flex-col justify-between items-center relative h-full p-5 sm:p-1 w-full">
-          <div className="flex-grow gap-1 flex flex-col justify-center items-center">
-            <div className="flex p-1">
-              <ImageSquare size={80} weight="thin" className="transform -rotate-12" />
-              <Video size={80} weight="thin" className="transform rotate-12" />
+         { selectedFile 
+            ?
+            <div className="flex-grow gap-3 flex flex-col justify-center items-center p-2">
+              <img src={selectedFile} alt="" className="w-full max-h-[350px] object-cover transition"/> 
+              <TrashSimple  onClick={() => setSelectedFile(null)} className="post-buttons hover:text-red-600 hover:bg-red-200 hover:scale-110 transition-all ease-in-out"/>
+            </div> 
+            : 
+            (
+            <div className="flex-grow gap-1 flex flex-col justify-center items-center">
+              <div className="flex">
+                <ImageSquare size={80} weight="thin" className="transform -rotate-12" />
+                <Video size={80} weight="thin" className="transform rotate-12" />
+              </div>
+              <h2 className="text-md">Drag photos and videos here</h2>
+              <input type="file" accept=".jpg, .jpeg, .png" hidden id="upload-button" onChange={(e) => addImageToPost(e)}/>
+              <label htmlFor="upload-button" className="font-semibold text-xs bg-sky-500 text-white p-3 rounded-lg cursor-pointer hover:bg-blue-500">Select from your device</label>
             </div>
-            <h2 className="text-md">Drag photos and videos here</h2>
-            <input type="file" accept=".jpg, .jpeg, .png" hidden id="upload-button" />
-            <label htmlFor="upload-button" className="font-semibold text-xs bg-sky-500 text-white p-3 rounded-lg cursor-pointer hover:bg-blue-500">Select from your device</label>
-          </div>
+            )
+          }
           <div className="flex gap-1 flex-col items-center p-2 w-full">
             <input type="text" placeholder="Enter your caption" className="focus:ring-0 border-none placeholder-shown:text-center text-center text-sm w-full" max={50}/>
             <button disabled className="font-semibold text-xs  text-white p-3 rounded-lg cursor-pointer disabled:cursor-not-allowed disabled:text-slate-500 disabled:bg-gray-300 w-1/4">Upload</button>
